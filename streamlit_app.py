3423206fd9c10e8153ef32b891cec5cb808f2821
papavask <44700690+papavask@users.noreply.github.com>
2024-12-30 15:34:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 with st.form("my_form"):
   user_input = st.text_input("Enter your E-Mail", "anyone@any.com")
-  st.form_submit_button(label="Submit")
+  st.form_submit_button(label="Submit", icon="🔥")
